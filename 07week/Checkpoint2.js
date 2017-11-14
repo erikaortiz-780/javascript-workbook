50b5ea6b35019ef2a843a00334151e3d30a579d9
Erika Ortiz <erikaortiz@Erikas-MacBook-Air.local>
2017-11-14 12:15:33 -0600

    checkpoint2

---
+++ 07week/Checkpoint2.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const userArray = [
+    {
+            "customer": {
+                "id": 1,
+                "customerName":"Marilyn Monroe",
+                "customerCity":"New York City",
+                "customerState":"NY",
+                "product":"Yellow Chair",
+                "productPrice": 19.99
+            }
+        },
+        {
+            "customer": {
+                "id": 2,
+                "customerName":"Abraham Lincoln",
+                "customerCity":"Boston",
+                "customerState":"MA",
+                "product":"Movie Tickets",
+                "productPrice": 27.00
+            }
+        },
+                {
+            "customer": {
+                "id": 3,
+                "customerName":"John F. Kennedy",
+                "customerCity":"Dallas",
+                "customerState":"TX",
+                "product":"Mustang Convertible",
+                "productPrice": 24999.99
+            }
+        },
+                {
+            "customer": {
+                "id": 4,
+                "customerName":"Martin Luther King",
+                "customerCity":"Burmingham",
+                "customerState":"AL",
+                "product":"Sandwiches",
+                "productPrice": 7.99
+            }
+        },
+];
+
+const client = userArray.map((person)=> {
+    return `${person.customer.customerName} paid $${person.customer.productPrice} for ${person.customer.product} in ${person.customer.customerCity}, ${person.customer.customerState}`
+});
+
+console.log(client);
